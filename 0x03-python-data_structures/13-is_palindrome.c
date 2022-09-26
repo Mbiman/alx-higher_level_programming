c743b83f366c1656e701afacfd209dd76219b198
mbiman <juniormbi80@gmail.com>
2022-09-26 22:26:51 +0100

    lists.h

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,55 +1,59 @@
 #include "lists.h"
 
 /**
- * is_palindrome - function to determine if singly linked list is a palindrome
- * @head: input pointer to head of singly linked list
- * Return: 1 if palindrome, 0 if not
+ * is_palindrome - Check if is palindrome
+ *
+ * @head: Head of Linked List
+ *
+ * Return: 0 if not palindrome, 1 if is palindrome
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp;
-	int count = 0;
 
-	if (head == NULL)
-		return (0);
-	if (*head == NULL)
-		return (1);
-	tmp = (*head);
-	while (tmp->next != NULL && tmp->next->next != NULL)
+	if (!*head || !(*head))
 	{
-		count += 2;
-		tmp = tmp->next->next;
+		return (1);
 	}
-	if (tmp->next != NULL)
-		count++;
-	tmp = (*head);
-	return (palindrome_check(tmp, count));
+
+	if (p_check(head, *head))
+	{
+		return (1);
+	}
+	return (0);
 }
 
 /**
- * palindrome_check - recursive function to check if listint list is palidrome
- * @tmp_head: pointer to the current head of the list
- * @count: integer count of how many nodes from head to check the end
- * Return: 1 if palindrome, 0 if not
+ *  p_check - Check for palindrome
+ *
+ *  @left: Go left
+ *
+ *  @right: Go right
+ *
+ *  Return: Integer
  */
 
-int palindrome_check(listint_t *tmp_head, int count)
-{
-	listint_t *tmp_end = tmp_head;
-	int count2 = 0;
 
-	/* move tmp_end to the end of where checking based on input count */
-	for (count2 = 0; count2 < count; count2++)
-		tmp_end = tmp_end->next;
-	/* count has successfully reached the middle (or past) of the list */
-	if (count <= 0)
+int p_check(listint_t **left, listint_t *right)
+{
+	int is_p = 0;
+
+	if (right)
+	{
+		is_p = p_check(left, right->next);
+	}
+	else
+	{
 		return (1);
-	/* a mismatch is found, no palindrome */
-	if (tmp_head->n != tmp_end->n)
-		return (0);
-	/* otherwise, not at end and matching, move forward and call again */
-	tmp_head = tmp_head->next;
-	count -= 2;
-	return (palindrome_check(tmp_head, count));
+	}
+
+	if (is_p == 1)
+	{
+		if ((*left)->n == right->n)
+		{
+			(*left) = (*left)->next;
+			return (1);
+		}
+	}
+	return (0);
 }
